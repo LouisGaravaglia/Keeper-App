3d4cf9deda860452a0bb3c28987babfb7a794d84
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-06-12 11:39:52 -0700

    added delete function which updates the note component array when a note is deleted

--- my-app/src/components/app.jsx
+++ my-app/src/components/app.jsx
@@ -4,7 +4,7 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./note";
 import CreateArea from "./CreateArea";
-import notes from "../notes";
+
 
 
 function App() {
--- my-app/src/components/note.jsx
+++ my-app/src/components/note.jsx
@@ -4,9 +4,15 @@ import ReactDOM from "react-dom";
 
 function Note(props) {
 return (
-    <div className="note">
+    <div 
+     className="note"
+    onClick={() => {
+        props.onChecked(props.id);
+    }}
+    >
+
     <h1>{props.title}</h1>
-<p> {props.content} </p>
+    <p> {props.content} </p>
     </div>
 
 )
