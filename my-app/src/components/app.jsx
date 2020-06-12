5d6c04eeda2c0cce17e9966e82edcd7cca34e299
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-06-12 10:48:56 -0700

    added items.map function to body of the app.js Note component

--- my-app/src/components/app.jsx
+++ my-app/src/components/app.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./note";
-import notes from "../notes";
 import CreateArea from "./CreateArea";
+import notes from "../notes";
 
 
 function App() {
+
+    const [items, setItems] = useState([])
 
     function addItem() {
         return (
@@ -20,16 +22,19 @@ function App() {
 
         )
     }
-    
+
 return (
     <div>
         <Header />
-        <CreateArea />
-        {notes.map(note =>
+        <CreateArea
+        onAdd={addItem}
+        />
+        {items.map((item, index) =>
         <Note
-        key={note.key}
-        title={note.title}
-        content={note.content}
+        key-{index}
+        id={index}
+        title={note.heading}
+        content={note.body}
         />
         )}
         <Footer />
