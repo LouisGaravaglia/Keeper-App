38278e61814f331a16ccfae10cd7cdcd073c1e3b
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-06-11 21:01:39 -0700

    set things back to the beginning

--- my-app/src/components/app.jsx
+++ my-app/src/components/app.jsx
@@ -1,26 +1,53 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./note";
-import notes from "../notes";
-
+import InputArea from "./InputArea";
 
 function App() {
-return (
-    <div>
-        <Header />
-        {notes.map(note =>
-        <Note
-        key={note.key}
-        title={note.title}
-        content={note.content}
-        />
-        )}
+  const [items, setItems] = useState([]);
+
+  function addItem(inputText) {
+    setItems(prevItems => {
+      return [...prevItems, inputText];
+    });
+  }
+
+  function deleteItem(id) {
+    setItems(prevItems => {
+      return prevItems.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <InputArea onAdd={addItem} />
+      <div>
+      <Header />
+        <ul>
+          {items.map((todoItem, index) => (
+            <Note
+              key={index}
+              id={index}
+              text={todoItem}
+              onChecked={deleteItem}
+              title={note.title}
+                content={note.content}
+            />
+          ))}
+        </ul>
         <Footer />
+      </div>
     </div>
-)
+  );
 }
+
 
 export default App;
 
